184c20203fbe769ecb0db9baecbb992af0d90cee
Daniel Lim <daniel.f.lim@gmail.com>
2024-12-10 11:37:08 -1000

    AWS authentication with keys

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,10 +4,17 @@
 // npm install @actions/core @actions/github @actions/exec
 
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
+// const github = require('@actions/github');
 
 function run() {
+	const bucket = core.getInput('bucket', { required: true });
+	const region = core.getInput('region', { required: true });
+	const dist_folder = core.getInput('dist-folder', { required: true });
+
+	const s3_uri = `s3://${bucket}`
+	exec.exec('aws s3 sync ${dist_folder} ${s3_uri} --region ${region}');
+
 	core.notice('Hello from my custom javascript action.');
 }
 
